65d5751e15dfb5ad2e0127a82525352d3ca82a0a
Kim <andrewk@ablgam.com>
2018-06-28 13:38:51 +0900

    baemin index view

--- final/final/urls.py
+++ final/final/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import  path
 from django.conf.urls import include, url
-
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	url(r'^accounts/', include('accounts.urls')),
-	url(r'^baemin/', include('baemin.urls', namespace='baemin')),
+	#url(r'^baemin/', include('baemin.urls', namespace='baemin')),
+	path('baemin/', include('baemin.urls'), name = 'baemin'),
+	url('', lambda r: redirect('baemin/'), name='root'),
+	
+	
 ]
