9d90891726768f2560d8a167c9fb4b67add9cb20
Kim <andrewk@ablgam.com>
2018-06-28 15:48:43 +0900

    baemin mainpage List

--- final/baemin/views.py
+++ final/baemin/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Shop
 # Create your views here.
 
 def index(request):
 	return render(request, 'baemin/index.html', {
+		'shop_list': Shop.objects.all(),
 	})
