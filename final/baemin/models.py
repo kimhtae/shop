fbc620426a38f94b97be22883a7a881e6f20e6de
Kim <andrewk@ablgam.com>
2018-07-03 09:55:27 +0900

    기본 정렬

--- final/baemin/models.py
+++ final/baemin/models.py
@@ -7,7 +7,10 @@ class Shop(models.Model):
 	name = models.CharField(max_length=100)
 	tel = models.CharField(max_length=20)
 	addr = models.CharField(max_length=100)
-	
+	# 정렬
+	class Meta:
+		ordering = ('-id',)
+		
 	def __str__(self):
 		return self.name
 		
@@ -15,7 +18,10 @@ class Item(models.Model):
 	shop = models.ForeignKey('Shop', on_delete=models.CASCADE)
 	name = models.CharField(max_length=100)
 	price = models.PositiveIntegerField()
-	
+	# 정렬
+	class Meta:
+		ordering = ('-id',)	
+		
 	def __str__(self):
 		return self.name
 
@@ -26,6 +32,10 @@ class Order(models.Model):
 	item_set = models.ManyToManyField(Item)
 	created_at = models.DateTimeField(auto_now_add=True)
 	
+	# 정렬
+	class Meta:
+		ordering = ('-id',)
+		
 	@property
 	def total(self):
 		return sum(item.price for item in self.item_set.all())
